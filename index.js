cc61bcbaa9b2438f6c2119adb9b5ccb81ca8bfe0
joykbiswas <joykumarbiswas100@gmail.com>
2023-11-05 23:14:11 +0600

    click btn and dataloaded from mongodb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -47,6 +47,14 @@ async function run() {
       res.send(result)
   })
 
+  app.get('/jobs/:id', async(req,res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await jobsCollection.findOne(query);
+    res.send(result)
+  })
+
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
