ae37dff0b441cde402f37f5fe4c6064f1c82110c
joykbiswas <joykumarbiswas100@gmail.com>
2023-11-05 12:23:49 +0600

    create api data and save mongodb

--- index.js
+++ index.js
@@ -25,10 +25,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const jobsCollection = client.db('JobsDB').collection('jobs');
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    // create
+    app.post('/Jobs', async(req, res) =>{
+      const newJobs=req.body;
+      console.log(newJobs);
+      const result = await jobsCollection.insertOne(newJobs)
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
