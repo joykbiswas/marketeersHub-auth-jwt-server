085a2b628bbd00e28b88903fec976d7ef6cb7720
joykbiswas <joykumarbiswas100@gmail.com>
2023-11-06 21:56:36 +0600

    token set cookie

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000;
 
 
 // MIDDLEWARE
-app.use(cors());
-app.use(express.json())
+app.use(cors({
+  origin:[
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqpfzla.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,12 +33,48 @@ const client = new MongoClient(uri, {
   }
 });
 
-const jobsCollection = client.db('JobsDB').collection('jobs');
-const applyJobsCollection = client.db('JobsDB').collection('apply');
+
+// middleware
+const logger = (req, res,next) =>{
+  console.log('log info:',req.method, req.url);
+  next()
+}
+
+// const verifyToken = (req, res, next) =>{
+
+// }
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const jobsCollection = client.db('JobsDB').collection('jobs');
+const applyJobsCollection = client.db('JobsDB').collection('apply');
+
+    
+  //auth related api
+  app.post('/jwt',logger, async(req, res) =>{
+    const user = req.body
+    console.log('user for token', user);
+    const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+      expiresIn:'1h'})
+      res.cookie('token',token, {
+        httpOnly:true,
+        secure:true,
+        sameSite:"none"
+      })
+      .send({success : true})
+
+  })
+
+  app.post('/logout', async(req, res) =>{
+    const user =req.body;
+    console.log("logging out", user);
+    res.clearCookie('token',{maxAge: 0}).send({success: true})
+  })
+
+
+  //server related api
 
     // create
     app.post('/Jobs', async(req, res) =>{
@@ -87,8 +131,9 @@ async function run() {
     res.send(result)
   })
 
-  // apply data loaded
-  app.get('/apply', async(req,res) =>{
+  // apply data loaded for user
+  app.get('/apply',logger, async(req,res) =>{
+    console.log("cook cookie",req.cookies);
     // console.log(req.query.email);
     let query = {}
     if(req.query?.email){
@@ -99,6 +144,18 @@ async function run() {
     res.send(result)
   })
 
+  
+  // apply data loaded for buyer
+  app.get('/apply',logger, async(req,res) =>{
+    // console.log(req.query.email);
+    let query = {}
+    if(req.query?.email){
+      query = {email: req.query?.email}
+    }
+    // console.log(query);
+    const result = await applyJobsCollection.find(query).toArray();
+    res.send(result)
+  })
 
 
   // delete
@@ -151,3 +208,8 @@ app.listen(port, () =>{
     console.log(`marketeersHub server is running on port: ${port}`);
 })
 
+
+
+//
+// throw new BSONError('input must be a 24 character hex string, 12 byte Uint8Array, or an integer');
+// ^
